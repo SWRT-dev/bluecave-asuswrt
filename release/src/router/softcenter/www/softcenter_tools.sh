1731faa157a57dea7873a144f431c22102198712
paldier <paldier@hotmail.com>
2019-02-09 11:45:23 +0800

    fix script

--- release/src/router/softcenter/www/softcenter_tools.sh
+++ release/src/router/softcenter/www/softcenter_tools.sh
@@ -1,25 +1,7 @@
 #! /bin/sh
 
-usb_uuid=`dbus get jffs_ext`
-if [ -n "$usb_uuid" ]; then
-mdisk=`blkid |grep "${usb_uuid}" |cut -c6-9`
-else
 mdisk=`nvram get k3c_disk`
-fi
-usb_disk="/tmp/mnt/$mdisk"
 usbmount=`ls /tmp/mnt/`
-
-check(){
-ndisk=`nvram get k3c_disk`
-nmount=`ls /tmp/mnt/ |grep $mdisk`
-if [ -n "$nmount" -a "$nmount" != "$mdisk" ]; then
-	uuid=`blkid |grep $ndisk |cut -d '"' -f 2`
-	dbus set jffs_ext="${uuid}"
-else if [ -n "$nmount" -a -z "$usb_uuid" ]; then
-	uuid=`blkid |grep $ndisk |cut -d '"' -f 2`
-	dbus set jffs_ext="${uuid}"
-fi
-}
 
 start(){
 #开机启动太早会卡？
@@ -31,7 +13,23 @@ do
 	sleep 5s
 	usbmount=`ls /tmp/mnt/ |grep $mdisk`
 done
-	check
+
+	usb_uuid=`dbus get jffs_ext`
+	if [ -n "$usb_uuid" ]; then
+		mdisk=`blkid |grep "${usb_uuid}" |cut -c6-9`
+	else
+		mdisk=`nvram get k3c_disk`
+	fi
+	usb_disk="/tmp/mnt/$mdisk"
+	nmount=`ls /tmp/mnt/ |grep $mdisk`
+	if [ -n "$nmount" -a "$nmount" != "$mdisk" ]; then
+		uuid=`blkid |grep $ndisk |cut -d '"' -f 2`
+		dbus set jffs_ext="${uuid}"
+	elif [ -n "$nmount" -a -z "$usb_uuid" ]; then
+		uuid=`blkid |grep $ndisk |cut -d '"' -f 2`
+		dbus set jffs_ext="${uuid}"
+	fi
+
 	if [ ! -e "$usb_disk" ]; then
 		k3c_warnning="1"
 		nvram set k3c_enable="0"
@@ -39,29 +37,51 @@ done
 		echo " $(date "+%F %T"):""没有找到可用的USB磁盘！" >> /tmp/k3c.log
 	fi
 	if [ "$k3c_warnning" != "1" ];then
-		if [ ! -d "/jffs/softcenter" -a ! -h "/jffs/softcenter" ] ;then
-
-			ln -s $usb_disk /jffs/softcenter
-			if [ ! -e "/jffs/softcenter/webs" ] ;then
-				mkdir -p /jffs/softcenter/bin
-				mkdir -p /jffs/softcenter/res
-				mkdir -p /jffs/softcenter/webs
-				mkdir -p /jffs/softcenter/scripts
-				mkdir -p /jffs/softcenter/etc
-				mkdir -p /jffs/softcenter/etc/init.d
-				if [ ! -e "/jffs/softcenter/webs/softcenter.xml" ] ;then
-				cp -rf /usr/sbin/softcenter.xml /jffs/softcenter/webs
+		if [ ! -d "/jffs/softcenter" ] ;then
+			mkdir -p /jffs/softcenter
+			mkdir -p $usb_disk/bin
+			mkdir -p $usb_disk/res
+			mkdir -p $usb_disk/webs
+			mkdir -p $usb_disk/scripts
+			mkdir -p /jffs/softcenter/etc
+			mkdir -p /jffs/softcenter/init.d
+			mkdir -p /jffs/softcenter/configs
+			mkdir -p /jffs/softcenter/ss
+			mkdir -p /jffs/softcenter/perp
+			if [ ! -e "/jffs/softcenter/init.d/S10softcenter.sh" ]; then
+				ln -s $usb_disk/bin /jffs/softcenter/bin
+				ln -s $usb_disk/res /jffs/softcenter/res
+				ln -s $usb_disk/webs /jffs/softcenter/webs
+				ln -s $usb_disk/scripts /jffs/softcenter/scripts
+				wget --no-check-certificate --timeout=10 --tries=3 -qO /tmp/softcenter.tar.gz http://sc.paldier.com/softcenter/softcenter.tar.gz
+				if [ "$?" == "0" ]; then
+					tar -zxvf /tmp/softcenter.tar.gz
+					cp -rf /tmp/softcenter/scripts/* /jffs/softcenter/scripts/
+					cp -rf /tmp/softcenter/res/* /jffs/softcenter/res/
+					cp -rf /tmp/softcenter/webs/* /jffs/softcenter/webs/
+					cp -rf /tmp/softcenter/bin/* /jffs/softcenter/bin/
+					cp -rf /tmp/softcenter/perp/* /jffs/softcenter/perp/
+					chmod 755 /jffs/softcenter/scripts/*.sh
+					chmod 755 /jffs/softcenter/bin/*
+					chmod 755 /jffs/softcenter/perp/*
+					ln -sf /jffs/softcenter/scripts/ks_app_install.sh /jffs/softcenter/init.d/S10softcenter.sh
+					cp -rf /jffs/softcenter/scripts/ks_app_install.sh /jffs/softcenter/scripts/ks_app_remove.sh
+					echo " $(date "+%F %T"):""安装完成，重启后生效！" >> /tmp/k3c.log
+				else
+					echo " $(date "+%F %T"):""下载失败！" >> /tmp/k3c.log
 				fi
 			fi
-		else
-			rm -rf /jffs/softcenter
-			ln -s $usb_disk /jffs/softcenter
 		fi
+		ln -s $usb_disk/bin /jffs/softcenter/bin
+		ln -s $usb_disk/res /jffs/softcenter/res
+		ln -s $usb_disk/webs /jffs/softcenter/webs
+		ln -s $usb_disk/scripts /jffs/softcenter/scripts
+		echo " $(date "+%F %T"):""挂载完成！" >> /tmp/k3c.log
 	fi
 }
 
 stop(){
-	rm -rf /jffs/softcenter
+	#rm -rf /jffs/softcenter
 	dbus remove jffs_ext
 }
 
