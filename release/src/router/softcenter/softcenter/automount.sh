f0c1f0a6e784746757e862e8cc8edd97a000cfd2
Xu Zhaowei <paldier@hotmail.com>
2020-06-04 11:03:41 +0800

    update watchdog

---
+++ release/src/router/softcenter/softcenter/automount.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mdisk=`nvram get sc_disk`
+cd /tmp/mnt
+if [ ! -d "$mdisk" ]; then
+	list_disk=`ls /tmp/mnt`
+	for cur_disk in $list_disk do
+		if [ -f "$cur_disk/.sc_installed" ]; then
+			ln -sf $cur_disk $mdisk
+		fi
+	done
+fi
+
